b1098bcec27a0eafeb64d7320deb4f428023d0d6
officialmyndlife-alt <official.myndlife@gmail.com>
2025-12-26 19:51:01 +0100

    Update paddle.ts

--- lib/paddle.ts
+++ lib/paddle.ts
@@ -1,1 +1,13 @@
+import { initializePaddle } from "@paddle/paddle-js";
 
+let paddlePromise: ReturnType<typeof initializePaddle> | null = null;
+
+export function getPaddle() {
+  if (!paddlePromise) {
+    paddlePromise = initializePaddle({
+      environment: process.env.NEXT_PUBLIC_PADDLE_ENV!,
+      token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN!,
+    });
+  }
+  return paddlePromise;
+}
